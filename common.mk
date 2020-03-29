d8bcae29ceaf71f549006aa90e3066fc9a0f73e1
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-03-29 09:56:41 +0900

    add make checkout for AppVeyor CI

--- common.mk
+++ common.mk
@@ -1249,6 +1249,14 @@ dist:
 	-unicode-version=$(UNICODE_VERSION) \
 	tmp $(RELNAME)
 
+checkout:: update-download
+
+checkout::
+	-$(Q)$(MAKE) $(mflags) Q=$(Q) REVISION_FORCE=PHONY "$(REVISION_H)"
+
+checkout::
+	-$(Q)$(MAKE) $(mflags) Q=$(Q) after-update
+
 up:: update-remote
 
 up::
