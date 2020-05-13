21ac2886296354af0cf5b14a5acaa9ff7b915c1d
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-05-14 00:56:19 +0900

    re:impl Array#map using rb_ary_collect_bang func

--- array.c
+++ array.c
@@ -3063,6 +3063,7 @@ rb_ary_sort_by_bang(VALUE ary)
     return ary;
 }
 
+static VALUE rb_ary_collect_bang(VALUE ary);
 
 /*
  *  call-seq:
@@ -3088,15 +3089,8 @@ rb_ary_sort_by_bang(VALUE ary)
 static VALUE
 rb_ary_collect(VALUE ary)
 {
-    long i;
-    VALUE collect;
-
-    RETURN_SIZED_ENUMERATOR(ary, 0, 0, ary_enum_length);
-    collect = rb_ary_new2(RARRAY_LEN(ary));
-    for (i = 0; i < RARRAY_LEN(ary); i++) {
-        rb_ary_push(collect, rb_yield(RARRAY_AREF(ary, i)));
-    }
-    return collect;
+    ary = rb_ary_dup(ary);
+    return rb_ary_collect_bang(ary);
 }
 
 
