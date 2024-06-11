9074813ed11b4348541dd98d9017a6effc7194f1
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-06-11 21:01:43 +0900

    Use same type for RNode_LIST and RNode_VALUES

--- rubyparser.h
+++ rubyparser.h
@@ -561,13 +561,7 @@ typedef struct RNode_ZLIST {
     NODE node;
 } rb_node_zlist_t;
 
-typedef struct RNode_VALUES {
-    NODE node;
-
-    struct RNode *nd_head;
-    long nd_alen;
-    struct RNode *nd_next;
-} rb_node_values_t;
+typedef rb_node_list_t rb_node_values_t;
 
 typedef struct RNode_HASH {
     NODE node;
